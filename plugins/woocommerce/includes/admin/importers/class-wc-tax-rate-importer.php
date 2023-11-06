a5c4ea0cfc12acf153f6b684c405b942c01b982a
Alfredo Sumaran <a.sumaran@gmail.com>
2023-11-06 17:03:00 -0500

    Check if variable is countable before usage

--- plugins/woocommerce/includes/admin/importers/class-wc-tax-rate-importer.php
+++ plugins/woocommerce/includes/admin/importers/class-wc-tax-rate-importer.php
@@ -146,8 +146,9 @@ class WC_Tax_Rate_Importer extends WP_Importer {
 		if ( false !== $handle ) {
 
 			$header = fgetcsv( $handle, 0, $this->delimiter );
+			$count = is_countable( $header ) ? count( $header ) : 0;
 
-			if ( 10 === count( $header ) ) {
+			if ( 10 === $count ) {
 
 				$row = fgetcsv( $handle, 0, $this->delimiter );
 
